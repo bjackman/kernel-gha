3d0c0eb5a20feba1b94dcb0e23c02fb6599fa025
Brendan Jackman <bhenryj0117@gmail.com>
2025-02-23 17:15:28 +0100

    Don't copy whole kernel build

--- build.sh
+++ build.sh
@@ -7,7 +7,15 @@ set -eux -o pipefail
 
 mkdir -p build
 # Dunno how to point vng to the kernel tree so just cd in a subshell.
-(cd linux/; ../virtme-ng/vng -b O=../build)
+(cd linux/; ../virtme-ng/vng --kconfig)
+# Build kernel
+make -s -j $(( $(nproc) * 2 )) -C linux
+# Just extract the important bits, Github is dumb and will copy the whole
+# artifact between jobs every time which takes minutes if you include the whole
+# build result.
+make -j $(( $(nproc) * 2 )) -C linux INSTALL_PATH=$PWD/build install
+# Make a symlink that always has the same name
+ln -s vmlinuz-$(make --no-print-directory -C linux kernelrelease) build/vmlinuz
 
 # -static is a simple way to workaround differences in the shared
 # library environment between host & guest.
