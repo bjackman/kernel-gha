e96e4c530acff7f7bc1b7ea1baa16264178e55a7
Brendan Jackman <bhenryj0117@gmail.com>
2025-02-23 21:36:43 +0100

    Revert "Debug"
    This reverts commit b425b3225cadcefe8192ddda2a9b1b165e0d2cb1.

--- test.sh
+++ test.sh
@@ -5,17 +5,9 @@
 
 mkdir -p image
 
-umask 0000
+tar -C image  --zstd -xf input-artifacts/rootfs/image.tar.zst
 
-tar --version
-
-sha1sum input-artifacts/rootfs/image.tar.zst
-zstd -d -c input-artifacts/rootfs/image.tar.zst | tar -C image --preserve-permissions -xf -
-tar --list -v --zstd -f input-artifacts/rootfs/image.tar.zst
-
-ls -l input-artifacts/kernel/kselftest/kselftest_install/mm
-
-unshare -r virtme-ng/vng \
+unshare -r virtme-ng/vng --verbose \
     --root image --user root --run input-artifacts/kernel/vmlinuz \
     --rwdir=/mnt=input-artifacts/kernel/kselftest/kselftest_install -- \
-        "cd /mnt/mm; ls -l ; bash ./run_vmtests.sh -t mmap"
+        "cd /mnt/mm; ./run_vmtests.sh -t mmap"
